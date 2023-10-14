ca7f3a5015fdb5c23c1c3003f8bb20747a507cc6
spncr-ai <147221067+spncr-ai@users.noreply.github.com>
2023-10-14 13:31:31 +0800

    Add files via upload

---
+++ LAST NA/mainfront/mainfront/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mainfront
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_menu_Click(object sender, EventArgs e)
+        {
+            Form menu = new menu();
+            menu.Show();
+            this.Hide();
+        }
+
+        private void btn_order_Click(object sender, EventArgs e)
+        {
+            Form order = new order();
+            order.Show();
+            this.Hide();
+        }
+    }
+}
